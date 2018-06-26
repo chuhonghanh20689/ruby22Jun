74d07d215c856837fae58589f1f0c3018de14ac2
hanhch <chuhonghanh20689@gmail.com>
2018-06-26 14:47:28 +0700

    finish chapter 5

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,11 @@ require 'rails/test_help'
 
 # Minitest::Reporters.use!
 class ActiveSupport::TestCase
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
 end
