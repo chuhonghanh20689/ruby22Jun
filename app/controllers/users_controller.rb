62b7e14d044b9a07e74c978f89d23f6dc810dcab
hanhch <chuhonghanh20689@gmail.com>
2018-07-05 16:48:00 +0700

    finish chapter 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,28 +30,14 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    # @user = User.new(user_params)
-
-    # respond_to do |format|
-    #   if @user.save
-    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
-    #     format.json { render :show, status: :created, location: @user }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
-  
-    @user = User.new(user_params)    # Not the final implementation!
-    
+    @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
-
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'sessions/new'
-  resources :users
+  
   get '/home', to: 'static_pages#home'
   get  '/help', to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  resources :account_activations, only: [:edit]
 end
